8d5c0447bdcc0468fa9bf16ac4e69548887fc704
AmelieCousineau <ameliecousineau5@gmail.com>
2025-10-05 20:31:16 -0400

    alpha

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -14,10 +14,20 @@ export default class Header {
     this.setOptions();
 
     document.querySelector('.js-toggle').addEventListener('click', this.toggleMenu.bind(this));
+
+    const menuElements = document.querySelectorAll('.js-menu');
+    for(let i=0; i<menuElements.length; i++){
+      let menuElement = menuElements[i];
+      menuElement.addEventListener('click', this.removeMenu.bind(this));
+    }
   }
 
   toggleMenu(){
     this.element.classList.toggle('nav-is-active');
+  }
+
+  removeMenu(){
+    this.element.classList.remove('nav-is-active');
   }
 
   setOptions() {
